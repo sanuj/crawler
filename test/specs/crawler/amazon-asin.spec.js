086dd8416e0e50f5d863c741d5a3c85074982ea4
sanuj_s <sanuj.sharma.in@gmail.com>
2017-03-26 15:11:57 +0530

    add tests for buy box curreny and seller url

--- test/specs/crawler/amazon-asin.spec.js
+++ test/specs/crawler/amazon-asin.spec.js
@@ -9,11 +9,35 @@ const project = any => path.resolve(path.resolve(__dirname, '../../../'), any)
 describe('amazon-asin', () => {
   describe('#getBuyBoxPrice()', () => {
     it('should return the buy box price', (done) => {
-      let $ = null
       util.readFromFile(project('test/stubs/asin-response.html'))
         .then(content => {
-          $ = cheerio.load(content)
+          const $ = cheerio.load(content)
           expect(amazon.getBuyBoxPrice($)).to.be.equal('222,492.00')
+
+          done() // Async test completed.
+        })
+    })
+  })
+
+  describe('#getBuyBoxCurrency()', () => {
+    it('should return the buy box currency', (done) => {
+      util.readFromFile(project('test/stubs/asin-response.html'))
+        .then(content => {
+          const $ = cheerio.load(content)
+          expect(amazon.getBuyBoxCurrency($)).to.be.equal('INR')
+
+          done() // Async test completed.
+        })
+    })
+  })
+
+  describe('#getBuyBoxSellerRedirectUrl()', () => {
+    it('should return the buy box seller redirect url', (done) => {
+      util.readFromFile(project('test/stubs/asin-response.html'))
+        .then(content => {
+          const $ = cheerio.load(content)
+          expect(amazon.getBuyBoxSellerRedirectUrl($)).to.be
+            .equal('/gp/help/seller/at-a-glance.html/ref=dp_merchant_link?ie=UTF8&seller=A1QF42CTKPRMM0&isAmazonFulfilled=1')
 
           done() // Async test completed.
         })
