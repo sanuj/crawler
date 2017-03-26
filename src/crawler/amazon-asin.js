cc10e5cfbece3ea450f3c0c7760c056a1b7de4b4
sanuj_s <sanuj.sharma.in@gmail.com>
2017-03-26 18:28:38 +0530

    crawl buy box seller info

--- src/crawler/amazon-asin.js
+++ src/crawler/amazon-asin.js
@@ -1,13 +1,12 @@
-'use strict'
-
-var axios = require('axios')
-var cheerio = require('cheerio')
-var logger = require('../logger.js')
-// var util = require('../util.js')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const logger = require('../logger.js')
+const urllib = require('url')
+// const util = require('../util.js')
 
 axios.defaults.timeout = 10000
 
-function makeUrl (type, params = null, base = 'http://www.amazon.in/') {
+function makeUrl (type, params = {}, base = 'http://www.amazon.in/') {
   const urls = {
     'base': base,
     'asin': base + 'dp/' + params,
@@ -17,34 +16,53 @@ function makeUrl (type, params = null, base = 'http://www.amazon.in/') {
   return urls[type]
 }
 
-var getBuyBoxSeller = $ => $('#merchant-info').find('a').attr('href')
-var getBuyBoxCurrency = $ => ($('#priceblock_ourprice').children().attr('class')).substring(8)
-var getBuyBoxPrice = $ => ($('#priceblock_ourprice').text()).trim()
+const getBuyBoxSellerRedirectUrl = $ => $('#merchant-info').find('a').attr('href')
+const getBuyBoxCurrency = $ => ($('#priceblock_ourprice').children().attr('class')).substring(8)
+const getBuyBoxPrice = $ => ($('#priceblock_ourprice').text()).trim()
+const getSellerInfoFromUrl = url => {
+  const query = urllib.parse(url, true).query
+  return {
+    seller_id: query.seller,
+    marketplace_id: query.marketplaceID
+  }
+}
+
+function crawlSellerInfoFromUrl (url) {
+  return new Promise((resolve, reject) => {
+    axios.get(makeUrl('base') + url)
+    .then(response => resolve(getSellerInfoFromUrl(response.request._currentUrl)))
+    .catch(reject)
+  })
+}
 
 function parseBuyBoxInfo (response) {
   // For debugging.
-  // writeInFile(__dirname + '/../../files/asin-response.html', response.data);
+  // writeInFile(__dirname + '/../../files/asin-response.html', response.data)
 
-  const $ = cheerio.load(response.data)
-  const buyboxseller = getBuyBoxSeller($)
-  const buyboxcurrency = getBuyBoxCurrency($)
-  const buyboxprice = getBuyBoxPrice($)
-  logger.info(buyboxseller)
-  logger.info(buyboxcurrency)
-  logger.info(buyboxprice)
+  return new Promise((resolve, reject) => {
+    const $ = cheerio.load(response.data)
+    crawlSellerInfoFromUrl(getBuyBoxSellerRedirectUrl($))
+      .then(sellerurl => resolve({
+        seller: sellerurl,
+        price: getBuyBoxPrice($),
+        currency: getBuyBoxCurrency($)
+      }))
+  })
 }
 
 function crawlAsin (asin) {
   // For debugging.
-  // util.readFromFile(__dirname + '/../../test/files/asin-response.html')
-  //   .then(data => parseBuyBoxInfo({data: data}));
+  // util.readFromFile(__dirname + '/../../test/stubs/asin-response.html')
+  //   .then(data => parseBuyBoxInfo({data: data})).then(console.log).catch(logger.error)
 
   axios.get(makeUrl('asin', asin))
     .then(parseBuyBoxInfo)
+    .then(logger.info)
     .catch(logger.error)
 }
 
 exports.crawlAsin = crawlAsin
-exports.getBuyBoxSeller = getBuyBoxSeller
+exports.getBuyBoxSellerRedirectUrl = getBuyBoxSellerRedirectUrl
 exports.getBuyBoxCurrency = getBuyBoxCurrency
 exports.getBuyBoxPrice = getBuyBoxPrice
+exports.getSellerInfoFromUrl = getSellerInfoFromUrl
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 const logger = require('./logger.js')
 
 logger.info('Foo Bar!')
+
+// For debugging.
+var amazon = require('./crawler/amazon-asin.js')
+amazon.crawlAsin('B01NCE2FHK')
